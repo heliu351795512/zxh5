cd90bf5f9c4bed6bb7b35c9c959f13768de5e941
heliu <jiarenyouyue@aliyun.com>
2022-03-06 17:51:53 +0800

    5

---
+++ static/config.js
@@ -0,0 +1,5 @@
+window.gconfig = {
+  API_URL: 'https://zxzqdev.winner123.cn',
+  API_SERVICE: '/g/zhongxin-meta-basics/v/',
+  BASE_PIC_PATH: 'static/imgs/'
+}
--- static/global.js
+++
@@ -1,4 +0,0 @@
-window.gmeta = {
-  'BASEURL': 'https://zxzqdev.winner123.cn/chinastocktjapi',
-  'API_PREFIX': '/g/zhongxin-meta-basics/v/'
-}
