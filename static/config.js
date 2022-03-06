a0be720500950b88dcf7b5701de53078cb099395
heliu351795512 <100930609+heliu351795512@users.noreply.github.com>
2022-03-06 18:55:35 +0800

    Update config.js

--- static/config.js
+++ static/config.js
@@ -1,5 +1,5 @@
 window.gconfig = {
-  API_URL: 'https://zxzqdev.winner123.cn',
+  API_URL: 'https://zxzqdev.winner123.cn/chinastocktjapi',
   API_SERVICE: '/g/zhongxin-meta-basics/v/',
   BASE_PIC_PATH: 'static/imgs/'
 }
